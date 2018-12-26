b8e7abb4a8fe96541d4b758797f918735ca6bde8
Kojiron987 <kojikojih987@gmail.com>
2018-12-26 22:29:31 +0900

    Kadai2.py: found bug

--- src/Kadai2.py
+++ src/Kadai2.py
@@ -1,38 +1,47 @@
 import math
 
-threshold = 10e-15
+threshold = 10e-10
+
+# FIXME: func1だと、find_threshold関数で Nが100000になっても誤差がthreshold
+# 以下に収まらず、hが増え続けるため誤差が大きくなり、無限ループにはまってしまう
 
 def func1(x):
-    return (x*x*x - 2*x*x + x - 1)
+    return (x**3 - 2*x**2 + x - 1)
 
 def func2(x):
     return math.cos(x)
 
 
 def solve_simpson(xmin, xmax, N, func):
-    S = 0
     h = (xmax - xmin) / (2 * N)
     x = xmin
 
-    while x < xmax:
-        y1 = func(x)
+    y0 = func(xmin)
+    y_odd = 0
+    y_even = 0
+    y2N = func(xmax)
+
+    for i in range(1, N - 1):
         x += h
-        y2 = func(x)
+        y_odd += func(x)
+
         x += h
-        y3 = func(x)
+        y_even += func(x)
 
-        S += (y1 + 4 * y2 + y3)
+    x += h
+    y_odd += func(x)
 
-    return S * (h / 3)
 
-def find_threshold(xmin, xmax, step, func):
-    N = step
+    return (h / 3) * (y0 + 4*y_odd + 2*y_even + y2N)
+
+def find_threshold(xmin, xmax, func):
+    N = 10
     while True:
         v = solve_simpson(xmin, xmax, N, func)
-        w = solve_simpson(xmin, xmax, N / 2, func)
+        w = solve_simpson(xmin, xmax, int(N / 2), func)
 
         if math.fabs(v - w) <= threshold:
             break
-        N += step
+        N *= 10
 
     return N
--- src/main.py
+++ src/main.py
@@ -18,11 +18,23 @@ if kadai == 'kadai1':
 
 elif kadai == 'kadai2':
 
-    import Kadai2
+    import Kadai2, math
 
     out_file = "../result/ans_Kadai1.txt"
 
+    xmin = -2
+    xmax = 3
+    N = 100000
+    func = Kadai2.func1
+    result1 = Kadai2.find_threshold(xmin, xmax, func)
+    print(f"func1 {result1}")
 
+    xmin = 0
+    xmax = math.pi / 2
+    N = 100
+    func = Kadai2.func2
+    result2 = Kadai2.find_threshold(xmin, xmax, func)
+    print(f"func2 {result2}")
 
 elif kadai == 'kadai3':
     out_file = "../result/ans_Kadai1.txt"
